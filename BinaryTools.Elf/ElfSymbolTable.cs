1fdd0ca5b9df73b9f74f948ec7e1d7d4ef6faf7b
Filip Jeremic <filip@jeremic.ca>
2020-02-27 20:45:29 -0500

    Add ElfSymbolTable and accompanying tests

---
+++ BinaryTools.Elf/ElfSymbolTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinaryTools.Elf
+{
+    public abstract class ElfSymbolTable : ElfSection, IReadOnlyList<ElfSymbolTableEntry>
+    {
+        /// <summary>
+        /// Gets the list of symbol entries in this symbol table.
+        /// </summary>
+        protected readonly List<ElfSymbolTableEntry> entries = new List<ElfSymbolTableEntry>();
+
+        /// <summary>
+        /// Gets a symbol entry at an index.
+        /// </summary>
+        /// 
+        /// <param name="index">
+        /// The index of the symbol entry.
+        /// </param>
+        /// 
+        /// <returns>
+        /// The symbol entry at the given index.
+        /// </returns>
+        public ElfSymbolTableEntry this[Int32 index]
+        {
+            get
+            {
+                return entries[index];
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of symbol entries in this section.
+        /// </summary>
+        public Int32 Count
+        {
+            get
+            {
+                return entries.Count;
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the symbol table entries.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// An enumerator that can be used to iterate through the symbol table entries.
+        /// </returns>
+        public IEnumerator<ElfSymbolTableEntry> GetEnumerator()
+        {
+            return entries.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the symbol table entries.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// An enumerator that can be used to iterate through the symbol table entries.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
