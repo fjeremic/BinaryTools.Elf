86228e6296aa88389de9905eb287c851247411d1
Filip Jeremic <filip@jeremic.ca>
2020-02-27 20:45:54 -0500

    Add ElfRelocationSection and accompanying tests

---
+++ BinaryTools.Elf/Bit32/ElfRelocationEntry.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace BinaryTools.Elf.Bit32
+{
+    internal sealed class ElfRelocationEntry : Elf.ElfRelocationEntry
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="ElfRelocationEntry"/> by extracting data from a <see cref="BinaryReader"/>.
+        /// </summary>
+        /// 
+        /// <param name="reader">
+        /// The reader used to extract the data needed to initialize this type.
+        /// </param>
+        /// 
+        /// <param name="position">
+        /// The position within the <paramref name="reader"/> base stream at which the entry begins.
+        /// </param>
+        /// 
+        /// <param name="hasAddend">
+        /// Determines whether to parse the addend field.
+        /// </param>
+        internal ElfRelocationEntry(BinaryReader reader, Int64 position, Boolean hasAddend)
+        {
+            reader.BaseStream.Position = position;
+
+            // Represents Elf32_Rel(a).r_offset
+            Offset = reader.ReadUInt32();
+
+            // Represents Elf32_Rel(a).r_info
+            Info = reader.ReadUInt32();
+
+            // Represents ELF32_R_SYM(i)
+            SymbolIndex = (Int32)(Info >> 8);
+
+            // Represents ELF32_R_TYPE(i)
+            Type = (UInt32)(Info & 0xFFUL);
+
+            if (hasAddend)
+            {
+                Addend = reader.ReadUInt32();
+            }
+
+            Symbol = String.Empty;
+        }
+    }
+}
---
+++ BinaryTools.Elf/ElfRelocationSection.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinaryTools.Elf
+{
+    public abstract class ElfRelocationSection : ElfSection, IReadOnlyList<ElfRelocationEntry>
+    {
+        /// <summary>
+        /// Gets the list of relocation entries in this relocation section.
+        /// </summary>
+        protected readonly List<ElfRelocationEntry> entries = new List<ElfRelocationEntry>();
+
+        /// <summary>
+        /// Gets the type value of a segment that is unused.
+        /// </summary>
+        public const UInt32 R_X86_64_JUMP_SLO = 0x00000007;
+
+        /// <summary>
+        /// Gets a relocation entry at an index.
+        /// </summary>
+        /// 
+        /// <param name="index">
+        /// The index of the relocation entry.
+        /// </param>
+        /// 
+        /// <returns>
+        /// The relocation entry at the given index.
+        /// </returns>
+        public ElfRelocationEntry this[Int32 index]
+        {
+            get
+            {
+                return entries[index];
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of relocation entries in this section.
+        /// </summary>
+        public Int32 Count
+        {
+            get
+            {
+                return entries.Count;
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the relocation section entries.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// An enumerator that can be used to iterate through the relocation section entries.
+        /// </returns>
+        public IEnumerator<ElfRelocationEntry> GetEnumerator()
+        {
+            return entries.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the relocation section entries.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// An enumerator that can be used to iterate through the relocation section entries.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
